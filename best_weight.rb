ae2065dbb5914d6314599b09ed94fed61ea3e48e
NicholasKrupenin <krupenin.nikolay@gmail.com>
2022-04-06 04:00:52 +0400

    First task ruby 1.0

---
+++ best_weight.rb
@@ -0,0 +1,10 @@
+puts "Hi man - pls write your name and height"
+print "Name -> " 
+name = gets.chomp.capitalize
+print "Height -> " 
+height = gets.to_i
+if (height-110)*1.15 <= 0
+    puts "Ok #{name} your weight is normal"
+else
+    puts " #{name}!!! you are very fat man, go to the gym "
+end
