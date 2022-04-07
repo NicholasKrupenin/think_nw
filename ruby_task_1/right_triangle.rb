bdcfccb13c083fc6cdc9888683818f4382d9e236
NicholasKrupenin <krupenin.nikolay@gmail.com>
2022-04-07 06:33:45 +0400

    I. - task ruby (ready)

--- ruby_task_1/right_triangle.rb
+++ ruby_task_1/right_triangle.rb
@@ -8,7 +8,7 @@ side_c = gets.to_i
 #
 #Searching equilateral or isosceles triangle
 #
-if  side_a == side_b && side_b == side_c 
+if  side_a == side_b && side_b == side_c
     puts "\n ~~> It's equilateral triangle"
 elsif side_a == side_b || side_b == side_c || side_a == side_c
     puts "\n It's isosceles triangle"
@@ -26,10 +26,7 @@ elsif side_b>side_a && side_b>side_c
 elsif side_c>side_a && side_c>side_b 
         if side_a**2+side_b**2 == side_c**2
         puts "\n ~~> it's right triangle, C - hypotenuse"
-        end
-
-elsif side_a != side_b && side_b != side_c && side_a != side_c
-        puts "\n ~~> it's " 
+        end     
 end
 
 
