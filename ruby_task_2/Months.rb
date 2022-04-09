300de4f0e0d7eefcbc50d2b7730f3c1f2baca280
NicholasKrupenin <krupenin.nikolay@gmail.com>
2022-04-09 08:59:10 +0400

    II. - task ruby (ready)

---
+++ ruby_task_2/Months.rb
@@ -0,0 +1,2 @@
+{January: 31,February: 28,March: 31,April: 30, May: 31,June: 30,July: 31,Augus: 31,September: 30,October: 31,November: 30,December: 31}.each {|i,k| puts "Month #{i} 30 day" if k == 30}
+
